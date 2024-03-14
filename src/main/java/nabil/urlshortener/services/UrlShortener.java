d11c40566ce2a26b9a18e4162fb1319e619cbfe0
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-03-14 19:55:11 +0200

    Enhance UrlShortener service with param checks

--- src/main/java/nabil/urlshortener/services/UrlShortener.java
+++ src/main/java/nabil/urlshortener/services/UrlShortener.java
@@ -4,10 +4,12 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import lombok.RequiredArgsConstructor;
 import nabil.urlshortener.domain.URL;
 import nabil.urlshortener.dtos.URLDTO;
+import nabil.urlshortener.exceptions.URLNotFoundException;
 import nabil.urlshortener.repositories.UrlRepository;
 
 @Service
@@ -19,12 +21,12 @@ public class UrlShortener {
     private final BijectiveFunction bijectiveFunction;
 
     public URLDTO shorten(String longUrl) {
-        // check if longUrl does not exist in db
+        assertNotEmpty(longUrl);
+        // if longUrl exist in db, return it (no duplicates allowed)
         Optional<URL> foundUrl = this.urlRepository.findByLongUrl(longUrl);
         if(foundUrl.isPresent()) {
             return new URLDTO(foundUrl.get());
         }
-        // save shortUrl in db
         URL savedUrl = this.urlRepository.save(new URL(longUrl));
         // generate shortUrl from id
         String shortUrl = bijectiveFunction.encode(savedUrl.getId());
@@ -34,6 +36,26 @@ public class UrlShortener {
     }
 
     public String expand(String shortUrl) {
-        return "longUrl";
+        assertNotEmpty(shortUrl);
+        assertLengthLessThanNine(shortUrl);
+        Long urlId = bijectiveFunction.decode(shortUrl);
+        URL url = this.urlRepository.findById(urlId).orElseThrow(URLNotFoundException::new);
+        return url.getLongUrl();
+    }
+
+    private void assertNotEmpty(String s) {
+        if(s == null) {
+            throw new IllegalArgumentException("URL cannot be null");
+        }
+        s = s.trim();
+        if(!StringUtils.hasText(s)) {
+            throw new IllegalArgumentException("URL cannot be empty");
+        }
+    }
+
+    private void assertLengthLessThanNine(String s) {
+        if(s.length() > 8) {
+            throw new IllegalArgumentException("URL is too long.");
+        }
     }
 }
