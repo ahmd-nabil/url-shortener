9aee63dd002fae9098cd2c805fd6293982499b8d
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-03-14 13:59:17 +0200

    Update URL entity

--- src/main/java/nabil/urlshortener/domain/URL.java
+++ src/main/java/nabil/urlshortener/domain/URL.java
@@ -3,10 +3,10 @@ package nabil.urlshortener.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +15,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
+@Table(name = "urls")
 public class URL {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "short_url", unique = true, nullable = false, length = 8)
+    @Column(name = "short_url", unique = true, nullable = true, length = 8)
     protected String shortUrl;
 
-    @Column(name = "long_url", unique = true, nullable = false, length = 10)
+    @Column(name = "long_url", unique = true, nullable = false, length = 255)
     private String longUrl;
+
+    public URL(String longUrl) {
+        this.longUrl = longUrl;
+    }
 }
