e588869a326b6769020c055e0199c52ebcdc4e7e
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-03-17 04:05:01 +0200

    Add OpenApi specs

--- src/main/java/nabil/urlshortener/controllers/URLController.java
+++ src/main/java/nabil/urlshortener/controllers/URLController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import nabil.urlshortener.dtos.URLDTO;
 import nabil.urlshortener.services.UrlService;
@@ -21,6 +26,14 @@ import nabil.urlshortener.services.UrlService;
 public class URLController {
     private final UrlService urlService;
 
+    @Operation(summary = "shortens a long url")
+    @Parameter(name = "longUrl", description = "original url to be shortened", required = true, example="https://www.example.com")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "",
+                content = { @Content(mediaType = "application/json") }),
+        @ApiResponse(responseCode = "400", description = "Invalid url supplied"),
+        @ApiResponse(responseCode = "500", description = "no url provided")
+    })
     @PostMapping(consumes = "text/plain", produces = "application/json")
     public ResponseEntity<URLDTO> shorten(@RequestBody String longUrl) {
         URLDTO urldto = urlService.shorten(longUrl);
@@ -28,6 +41,12 @@ public class URLController {
     }
 
 
+    @Operation(summary = "redirects to the original url, given the short url")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "302", description = "redirects to original url"),
+        @ApiResponse(responseCode = "400", description = "invalid url supplied"),
+        @ApiResponse(responseCode = "500", description = "no short url provided")
+    })
     @GetMapping("/{shortURL}")
     public ResponseEntity<Void> redirect(@PathVariable String shortURL) {
         String longUrl = urlService.expand(shortURL);
